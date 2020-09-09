ad79f31349654486fb6de58f0271f92402d849ed
tatico <huhy1099@foxmail.com>
2020-09-09 11:42:40 +0800

    init

---
+++ src/test/java/indi/huhy/demo/template/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package indi.huhy.demo.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
