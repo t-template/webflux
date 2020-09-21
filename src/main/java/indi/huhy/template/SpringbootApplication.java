0275448c41361b534b9a35335880fb6ebc4103ec
Tatico <huhy1099@foxmail.com>
2020-09-21 11:47:37 +0800

    no message

--- src/main/java/indi/huhy/template/SpringbootApplication.java
+++ src/main/java/indi/huhy/template/SpringbootApplication.java
@@ -1,8 +1,10 @@
 package indi.huhy.template;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("indi.huhy.template")
 @SpringBootApplication
 public class SpringbootApplication {
 
